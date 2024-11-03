56519e0b5c2fe08196436e468b6c63d10ff4ddce
Ankita Saha <154684995+Ankita-ASR@users.noreply.github.com>
2024-11-03 20:29:28 +0600

    Update B_Chat_Order.cpp

--- W-1/D-7/B_Chat_Order.cpp
+++ W-1/D-7/B_Chat_Order.cpp
@@ -1,32 +1,26 @@
-#include <bits/stdc++.h>
+ #include<bits/stdc++.h>
 using namespace std;
-
-void solve(int n) {
- deque<string> chat;  
- unordered_set<string> s; 
-
-    for (int i = 0; i < n; ++i) {
-      string Name;
-      cin >> Name; 
-  if (s.find(Name) != s.end()) {
-  auto it = find(chat.begin(), chat.end(), Name);
-   if (it != chat.end()) {
-    chat.erase(it);  
-   }
-    chat.push_front(Name); 
-     } 
-     else {
-     chat.push_front(Name);
-    s.insert(Name); 
-        }
-    }
- for (auto Name : chat) {
-   cout <<Name<< endl;  
-    }
-}
-
 int main() {
-    int n;
-    cin >> n;  
-    solve(n); 
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int n;
+   cin >> n;
+   vector<string> v;
+   map<string, bool> mp;
+   while (n--) {
+    string s;
+    cin >> s;
+    v.push_back(s);
+   }
+   reverse(v.begin(), v.end());
+   vector<string> result;
+   for (auto x : v) {
+    if (mp.find(x) == mp.end()) {
+        mp[x] = true;
+        result.push_back(x);
+    }
+   }
+    for (auto ans : result) {
+    cout << ans << '\n';
+    }
 }
