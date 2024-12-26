931bad18f158bf6bb5373e9b54fbde1404efc4d8
Ankita Saha <154684995+Ankita-ASR@users.noreply.github.com>
2024-12-26 23:33:27 +0600

    Add files via upload

---
+++ W-9/D-4/A_LCM_Problem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int l, r;
+        cin >> l >> r;
+
+        if (l * 2 <= r) {
+            cout << l << " " << 2 * l << "\n";
+        } else {
+            cout << "-1 -1\n";
+        }
+    }
+}
